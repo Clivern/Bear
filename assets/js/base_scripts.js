5171da0e7a4ebba787d4bee4b7398887b97c98f0
clivern <hello@clivern.com>
2016-06-11 20:33:29 +0200

    styles and scripts added

---
+++ assets/js/base_scripts.js
@@ -0,0 +1,42 @@
+/**
+ * Base Scripts
+ *
+ * @author   clivern
+ * @since    1.0.0
+ */
+
+var bear_theme = bear_theme || {};
+
+
+/*!
+ * Bear Utils Module
+ */
+bear_theme.utils = (function (window, document, $) {
+    'use strict';
+
+    var base = {
+        init : function() {
+            //
+        },
+    };
+    return {
+        init: base.init,
+    };
+})(window, document, jQuery);
+
+
+/*!
+ * Bear Customizer Module
+ */
+bear_theme.customizer = (function (window, document, $) {
+    'use strict';
+
+    var base = {
+        init : function() {
+            //
+        },
+    };
+    return {
+        init: base.init,
+    };
+})(window, document, jQuery);
---
+++ inc/modules/container.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Container Module
+ *
+ * @author   clivern
+ * @since    1.0.0
+ * @package  bear
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+if ( ! class_exists( 'BearContainer' ) ) :
+
+    /**
+     * The container class
+     */
+    class BearContainer {
+
+    }
+
+endif;
